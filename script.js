d800c30f96aae7344c2cd1c394140ca6a7ed1165
Beez <ayeoman41@gmail.com>
2023-02-25 10:45:59 -0500

    variables added to function

---
+++ script.js
@@ -0,0 +1,6 @@
+function (){
+    let screen = document.querySelector('.screen');
+    let buttons = document.querySelector('.btn');
+    let red = document.querySelector('.btn-red');
+    let green = document.querySelector('.btn-green');
+})();
