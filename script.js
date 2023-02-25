234693f19f1f5224d604fcc0347b373f927acbbd
Beez <ayeoman41@gmail.com>
2023-02-25 10:52:04 -0500

    button function added

--- script.js
+++ script.js
@@ -3,4 +3,11 @@ function (){
     let buttons = document.querySelector('.btn');
     let red = document.querySelector('.btn-red');
     let green = document.querySelector('.btn-green');
+
+    buttons.forEach(function(button) {
+        button.addEventListener('click', function(e) {
+            let value = e.target.dataset.num;
+            screen.value += value;
+        }
+    }
 })();
